b8769518514ef64ad8416e64e320e1bd3d08c414
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-02 15:15:34 -0400

    Using Vite to Handle Environment Variables - thanks to this link, i accomplished exactly what i desired to do in my project:         - https://vitejs.dev/guide/env-and-mode.html#env-variables         - not only i'm i using environment variables         - i am also calling on different environment variables based on whether my app is in development or production         - if i'm developing, run my localhost server, and when i deploy my app on production, run my production server...         - ... which i have not yet deployed to Heroku

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,13 +1,20 @@
 import { defineStore } from "pinia";
-import config from "../../env.json"; //NOTE: name can be anything, not just config
 
-/**NOTE:
- * even though i won't be moving forward with this solution in this project...
- * ... it's quite useful to keep in mind
- * found solution thanks to this link: https://stackoverflow.com/questions/72922941/cant-use-env-file-js
- * this link helped in the research as well: https://stackoverflow.com/questions/34944099/how-to-import-a-json-file-in-ecmascript-6
- */
-const URL = `${config.development.SERVER_URL}/products`;
+const isDevelopment = import.meta.env.DEV;
+const isProduction = import.meta.env.PROD;
+
+const devServerURL = import.meta.env.VITE_DEV_SERVER_URL;
+const prodServerURL = import.meta.env.VITE_PROD_SERVER_URL;
+
+console.log(`Development Mode: ${isDevelopment}`);
+console.log(`Production Mode: ${isProduction}`);
+
+console.log(import.meta.env.VITE_TEST_KEY); // NOTE: will work
+console.log(import.meta.env.KEY_TEST); // NOTE: will NOT work; not prepended with VITE_
+
+const serverURL = isDevelopment ? devServerURL : prodServerURL;
+
+const URL = `${serverURL}/products`;
 
 export const useProductsStore = defineStore("products", {
     state: () => {
